b8eb6bfc9ce443b28399688e393a228ceeb3f3b5
Akanksh Rao <akankshrao@akankshs-mbp.lan>
2026-02-02 18:27:17 -0500

    Added all the pages accordingly

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import './App.css'
 import { HomePage } from './pages/HomePage'
 import {Routes, Route} from 'react-router';
 import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
 
 function App() {
   return (
@@ -10,8 +12,14 @@ function App() {
       </Route>
 
       <Route path = "/checkout" element = {<CheckoutPage />}>
-        
       </Route>
+
+       <Route path = "/orders" element = {<OrdersPage />}>
+      </Route>
+
+       <Route path = "/tracking" element = {<TrackingPage />}>
+      </Route>
+
     </Routes>
   )
 }
