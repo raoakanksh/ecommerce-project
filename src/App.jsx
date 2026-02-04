27ec0a1c3a1db266fe60c30ee4159c7215187568
Akanksh Rao <akankshrao@akankshs-mbp.lan>
2026-02-04 16:09:22 -0500

    Generate Html element for cart items

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
-import './App.css'
-import { HomePage } from './pages/HomePage'
-import {Routes, Route} from 'react-router';
-import { CheckoutPage } from './pages/CheckoutPage';
-import { OrdersPage } from './pages/OrdersPage';
-import { TrackingPage } from './pages/TrackingPage';
+import "./App.css";
+import { HomePage } from "./pages/HomePage";
+import { Routes, Route } from "react-router";
+import { CheckoutPage } from "./pages/CheckoutPage";
+import { OrdersPage } from "./pages/OrdersPage";
+import { TrackingPage } from "./pages/TrackingPage";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/api/cart-items?expand=product")
+      .then((response) => {
+        setCart(response.data);
+      });
+  }, []);
+
   return (
     <Routes>
-      <Route path= "/" element={<HomePage />}> 
-      </Route>
+      <Route path="/" element={<HomePage cart={cart} />}></Route>
 
-      <Route path = "/checkout" element = {<CheckoutPage />}>
-      </Route>
+      <Route path="/checkout" element={<CheckoutPage cart={cart} />}></Route>
 
-       <Route path = "/orders" element = {<OrdersPage />}>
-      </Route>
+      <Route path="/orders" element={<OrdersPage />}></Route>
 
-       <Route path = "/tracking" element = {<TrackingPage />}>
-      </Route>
-
+      <Route path="/tracking" element={<TrackingPage />}></Route>
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,17 +3,12 @@ import { Header } from "../components/Header";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-export function HomePage() {
+export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     axios.get("http://localhost:3000/api/products").then((response) => {
       setProducts(response.data);
-    });
-
-    axios.get("http://localhost:3000/api/cart-items").then((response) => {
-      setCart(response.data);
     });
   }, []);
 
